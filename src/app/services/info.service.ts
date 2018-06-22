658df4a2eeeb167184c68d56e8a467b507902e2c
leonellrodriguez@gmail.com <Leonel.Rodriguez@BBASEGUROS.local>
2018-06-22 16:07:41 -0500

    COnexion Firebase

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -7,15 +7,30 @@ import { Http } from '@angular/http';
 export class InfoService {
 
   info:any = {};
+  equipo:any[] = [];
   cargado:boolean = false;
+  cargado_sobre_nosotros:boolean = false;
 
   constructor( public http:Http ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();    
 
-    this.http.get("assets/data/info.pagina.json")
-              .subscribe( data => {
-                console.log(data.json());
-                this.info = data.json();
-                this.cargado = true;
-              })
    }
+
+  public carga_info(){
+      this.http.get("assets/data/info.pagina.json")
+                .subscribe( data => {
+                  this.info = data.json();
+                  this.cargado = true;
+                })
+  }
+
+  public carga_sobre_nosotros(){
+      this.http.get("https://pruebaangular2-190bc.firebaseio.com/equipo.json")
+      .subscribe( data => {
+        console.log(data.json());
+        this.equipo = data.json();
+        this.cargado_sobre_nosotros = true;
+    })
+  }
 }
