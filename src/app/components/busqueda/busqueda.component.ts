d402f01b70826c4c1806503fb5bda4f62d5ccd72
leonellrodriguez@gmail.com <Leonel.Rodriguez@BBASEGUROS.local>
2018-06-26 11:11:05 -0500

    Final del curso

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { ProductosService } from "../../services/productos.service";
 
 @Component({
   selector: 'app-busqueda',
@@ -6,7 +8,15 @@ import { Component } from '@angular/core';
 })
 export class BusquedaComponent {
 
-  constructor() { }
+  param:string = undefined;
 
+  constructor( private route:ActivatedRoute,
+               private _ps:ProductosService ) { 
 
+    route.params.subscribe( parametros => {
+
+        this.param = parametros['param'];
+        _ps.buscar_producto( this.param );
+    })
+  }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean;
 
   constructor( public http:Http) {
@@ -15,17 +17,48 @@ export class ProductosService {
       this.cargar_productos();
    }
 
+   public buscar_producto( param:string ){
+
+        if( this.productos.length === 0 ){
+            this.cargar_productos().then( () => {
+                this.filtrar_productos( param );
+            });
+        }else{
+            this.filtrar_productos( param );
+        }
+   }
+
+   private filtrar_productos( param:string ){
+
+        this.productos_filtrado = [];
+        param = param.toLocaleLowerCase();
+
+        this.productos.forEach( prod => {
+
+            if( prod.categoria.indexOf( param ) >= 0 || prod.titulo.toLocaleLowerCase().indexOf( param ) >= 0 ) {
+                this.productos_filtrado.push( prod );
+                
+            }
+        })       
+   }
+
    public cargar_productos() {
 
-      this.http.get('https://pruebaangular2-190bc.firebaseio.com/productos_idx.json')
-          .subscribe( res => {
+        let promesa = new Promise( (resolve, reject) => {
 
-              //console.log(res.json());
-              //setTimeout( () => {
-                  this.cargando = false;
-                  this.productos = res.json();
-              //},1500)
-          })
+            this.http.get('https://pruebaangular2-190bc.firebaseio.com/productos_idx.json')
+            .subscribe( res => {
+
+                //console.log(res.json());
+                //setTimeout( () => {
+                    this.cargando = false;
+                    this.productos = res.json();
+                //},1500)
+                resolve();
+            })
+        })
+        return promesa;
+
    }
 
    public datosProducto( cod:string ) {
